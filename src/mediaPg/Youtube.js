bb03c1c50fcbc27a4b28773c2110b3e461a1f362
pkavya09 <kavyapendyala09@gmail.com>
2025-02-18 22:09:07 +0530

    file updated

--- src/mediaPg/Youtube.js
+++ src/mediaPg/Youtube.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
 function Youtube() {
 
     return ( 
        
         <div className='container m-auto p-5  ' >
         <div className='row m-auto' >
-        {/* style={{border:'4px solid grey',borderRadius:"20px",height:'500px',width:'80%',backgroundColor:'black'}}
-            <h1 style={{fontSize:'80px',color:'grey'}}>AdTask.ai</h1> */}
             <img src='/media/yt.png' alt="yt-img" 
             className='p-2' 
             style={{borderRadius:'20px',width:'70%',
